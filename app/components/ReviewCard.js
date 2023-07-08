f3c09347de3d9ca43142d2bdda27fb48a3c316f4
Terry Babyuk <tbabyuk@gmail.com>
2023-07-08 18:56:37 -0400

    completed reviews section, successfully fetching review data from Google Places API and rendering them on Reviews Page

---
+++ app/components/ReviewCard.js
@@ -0,0 +1,22 @@
+
+import {AiFillStar} from "react-icons/ai"
+
+
+const ReviewCard = ({review}) => {
+
+    const numStars = Array.from({length: review.rating})
+
+    return ( 
+        <div className="card flex grid-cols-2 p-5 mb-5 rounded-md bg-gradient-to-r from-gray-50 to-gray-100 shadow-md">
+            <div><img src={review.profile_photo_url} style={{maxWidth: "40px"}} alt="customer profile image" /></div>
+            <div className="px-5">
+                <p className="font-bold pb-1">{review.author_name}</p>
+                <p className="text-xs pb-1">{review.relative_time_description}</p>
+                <p className="flex pb-2">{numStars.map((star) => <AiFillStar color="#FB8E28" size="1.2em" />)}</p>
+                <p>{review.text}</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default ReviewCard;
--- app/reviews/page.js
+++ app/reviews/page.js
@@ -1,35 +1,30 @@
-"use client"
 
-import { useEffect, useState } from "react"
+import ReviewCard from "../components/ReviewCard";
+import Link from "next/link";
 
-// const fetchReviews = async () => {
-//      const res = await fetch('https://.................')
-//     const data = await res.json()
-// } 
 
+const fetchReviews = async () => {
+     const res = await fetch('https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJxzCGYLs3K4gR0hG4dhYU8mk&key=AIzaSyCefFIRmPQH-NNh_c2XOBNrHdulUH1eEfQ&fields=reviews&maxheight=10')
+
+    const data = await res.json()
+    return (
+        data.result.reviews
+    )
+} 
 
 
 const Reviews = async () => {
-    // const reviews = await fetchReviews()
+    const reviews = await fetchReviews()
 
-    const [reviews, setReviews] = useState([])
-
-    // console.log(reviews)
-
-    // useEffect(() => {
-    //     const fetchReviews = async () => {
-    //         const res = await fetch('https://.................')
-    //         // const res = await fetch("https://api.github.com/users/bradtraversy/repos")
-    //         const data = await res.json()
-    //         setReviews(data)
-    //     }     
-    //     fetchReviews()
-    // }, [])
+    // console.log("logging reviews", reviews)
 
     return ( 
-        <>
-        <h2>Reviews go here</h2>
-        </>
+        <main className="reviews-page px-5 md:px-20 lg:px-40 xl:px-80">
+            {reviews && 
+                reviews.map((review) => <ReviewCard review={review} />
+            )}
+            <div className="text-center mt-10"><Link href="https://search.google.com/local/reviews?placeid=ChIJxzCGYLs3K4gR0hG4dhYU8mk" target="_blank" className="hover:text-lightRed">See All Reviews...</Link></div>
+        </main>
      );
 }
  
