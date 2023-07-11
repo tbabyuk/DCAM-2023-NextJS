3b86dca82ec781a8c97dbe0bc234787c2316f11d
Terry Babyuk <tbabyuk@gmail.com>
2023-07-11 02:27:41 -0400

    added apply directive for styling buttons

--- app/components/TrialButton.js
+++ app/components/TrialButton.js
@@ -1,8 +1,6 @@
 "use client"
 
-
 import { useState, useRef } from "react";
-
 
 export const TrialButton = () => {
 
@@ -27,80 +25,77 @@ export const TrialButton = () => {
     }
 
 
-
-
     return ( 
         <>
-            <button className="bg-regGreen py-3 px-6 rounded text-gray-100 text-lg" onClick={() => setModalIsOpen(true)}>Free Trial Lesson</button>
-
+            <button className="dcam-button text-lg" onClick={() => setModalIsOpen(true)}>Free Trial Lesson</button>
 
             {modalIsOpen && (
-                <div className="backdrop fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-70 flex justify-center items-center" onClick={(e) => handleCloseModal(e)}>
-                    <span className="exit absolute top-0 right-5 sm:right-10 text-white text-7xl cursor-pointer">&times;</span>
-                    <div className="modal w-11/12 sm:w-8/12 md:w-6/12 lg:w-5/12 xl:w-4/12 2xl:w-3/12 px-5 sm:px-10 py-7 mt-16 bg-white text-black">
-                        <form className="flex flex-col" onSubmit={handleSubmit}>
-                            <h2 className="mb-5 text-center font-bold text-xl">FREE TRIAL LESSON</h2>
-                            <label className="mb-4">
-                                <span className="block text-sm">Student Name:</span>
-                                <input 
-                                    type="text" 
-                                    className="w-full h-8 border-2 border-gray-300 ps-2 text-sm" 
-                                    ref={studentNameRef} 
-                                />
-                            </label>
-                            <label className="mb-4">
-                                <span className="block text-sm">Parent/Guardian Name:</span>
-                                <input 
-                                    type="text" 
-                                    className="w-full h-8 border-2 border-gray-300 ps-2 text-sm"
-                                    ref={parentNameRef}
-                                />
-                            </label>
-                            <label className="mb-4">
-                                <span className="block text-sm">Desired Instrument:</span>
-                                <select className="w-full h-8 border-2 border-gray-300 text-sm" ref={instrumentRef}>
-                                    <option value="instrument">choose instrument</option>
-                                    <option value="piano">piano</option>
-                                    <option value="guitar">guitar</option>
-                                    <option value="ukulele">ukulele</option>
-                                    <option value="drums">drums</option>
-                                    <option value="voice">voice</option>
-                                    <option value="theory">music theory</option>
-                                </select>
-                            </label>
-                            <label className="mb-4">
-                                <span className="block text-sm">Phone:</span>
-                                <input 
-                                    type="tel"
-                                    className="w-full h-8 border-2 border-gray-300 ps-2 text-sm"
-                                    ref={phoneRef}
-                                />
-                            </label>
-                            <label className="mb-4">
-                                <span className="block text-sm">Email:</span>
-                                <input 
-                                    type="email"
-                                    className="w-full h-8 border-2 border-gray-300 ps-2 text-sm"
-                                    ref={emailRef}
-                                />
-                            </label>
-                            <label className="mb-4">
-                                <span className="block text-sm">How did you hear about us?</span>
-                                <select className="w-full h-8 border-2 border-gray-300 text-sm" ref={sourceRef}>
-                                    <option value="instrument" disabled>choose option</option>
-                                    <option value="google">google search</option>
-                                    <option value="humbertown">humbertown sign</option>
-                                    <option value="social">social media (FB/IG)</option>
-                                    <option value="flyer">print flyer</option>
-                                    <option value="blog">blog article</option>
-                                    <option value="other">other</option>
-                                </select>
-                            </label>
-                            <button className="w-full mt-3 bg-regGreen h-10 text-gray-50 rounded">Submit</button>
-                        </form>
-                    </div>
-                </div>
-            )
+        <div className="backdrop fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-70 flex justify-center items-center" onClick={(e) => handleCloseModal(e)}>
+        <span className="exit absolute top-0 right-5 sm:right-10 text-white text-7xl cursor-pointer">&times;</span>
+        <div className="modal w-11/12 sm:w-8/12 md:w-6/12 lg:w-5/12 xl:w-4/12 2xl:w-3/12 px-5 sm:px-10 py-7 mt-16 bg-white text-black">
+            <form className="flex flex-col" onSubmit={handleSubmit}>
+                <h2 className="mb-5 text-center font-bold text-xl">FREE TRIAL LESSON</h2>
+                <label className="mb-4">
+                    <span className="block text-sm">Student Name:</span>
+                    <input 
+                        type="text" 
+                        className="w-full h-8 border-2 border-gray-300 ps-2 text-sm" 
+                        ref={studentNameRef} 
+                    />
+                </label>
+                <label className="mb-4">
+                    <span className="block text-sm">Parent/Guardian Name:</span>
+                    <input 
+                        type="text" 
+                        className="w-full h-8 border-2 border-gray-300 ps-2 text-sm"
+                        ref={parentNameRef}
+                    />
+                </label>
+                <label className="mb-4">
+                    <span className="block text-sm">Desired Instrument:</span>
+                    <select className="w-full h-8 border-2 border-gray-300 text-sm" ref={instrumentRef}>
+                        <option value="instrument">choose instrument</option>
+                        <option value="piano">piano</option>
+                        <option value="guitar">guitar</option>
+                        <option value="ukulele">ukulele</option>
+                        <option value="drums">drums</option>
+                        <option value="voice">voice</option>
+                        <option value="theory">music theory</option>
+                    </select>
+                </label>
+                <label className="mb-4">
+                    <span className="block text-sm">Phone:</span>
+                    <input 
+                        type="tel"
+                        className="w-full h-8 border-2 border-gray-300 ps-2 text-sm"
+                        ref={phoneRef}
+                    />
+                </label>
+                <label className="mb-4">
+                    <span className="block text-sm">Email:</span>
+                    <input 
+                        type="email"
+                        className="w-full h-8 border-2 border-gray-300 ps-2 text-sm"
+                        ref={emailRef}
+                    />
+                </label>
+                <label className="mb-4">
+                    <span className="block text-sm">How did you hear about us?</span>
+                    <select className="w-full h-8 border-2 border-gray-300 text-sm" ref={sourceRef}>
+                        <option value="instrument" disabled>choose option</option>
+                        <option value="google">google search</option>
+                        <option value="humbertown">humbertown sign</option>
+                        <option value="social">social media (FB/IG)</option>
+                        <option value="flyer">print flyer</option>
+                        <option value="blog">blog article</option>
+                        <option value="other">other</option>
+                    </select>
+                </label>
+                <button className="dcam-button w-full mt-3 h-10">Submit</button>
+            </form>
+        </div>
+    </div>
+              )
             }
         </>
      );
