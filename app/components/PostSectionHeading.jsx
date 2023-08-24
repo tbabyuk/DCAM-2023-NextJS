345bc09e5c4aa8bc3cc1e36f78f82ad76e029221
Terry Babyuk <tbabyuk@gmail.com>
2023-08-24 00:26:33 -0400

    finished 'Easy Sonatinas for Piano Students' blog article

--- app/components/PostSectionHeading.jsx
+++ app/components/PostSectionHeading.jsx
@@ -8,6 +8,6 @@
 export const PostSectionHeading = ({heading}) => {
 
     return (
-          <h3 className="font-ovo text-regRed text-2xl self-start mb-8">{heading}</h3>
+          <h3 className="font-ovo text-regRed text-3xl self-start mb-8">{heading}</h3>
       )
   }
