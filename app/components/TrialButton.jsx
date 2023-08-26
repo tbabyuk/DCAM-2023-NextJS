99adfd5a6c44423770e9284412bbf05a22bde479
Terry Babyuk <tbabyuk@gmail.com>
2023-08-26 12:06:51 -0400

    fixed trial modal width responsiveness issue, fixed defaultValue bug on select element

--- app/components/TrialButton.jsx
+++ app/components/TrialButton.jsx
@@ -55,7 +55,7 @@ return (
         {modalIsOpen && (
             <div className="backdrop fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-70 flex justify-center items-center" onClick={(e) => handleCloseModal(e)}>
             <span className="exit absolute top-0 right-6 md:right-10 text-white text-7xl cursor-pointer">&times;</span>
-                <div className="modal w-[85%] sm:w-[380px] px-5 sm:px-10 py-7 mt-8 bg-gray-100 text-black rounded">
+                <div className="modal w-[85%] max-w-[380px] px-5 sm:px-10 py-7 mt-7 md:mt-2 bg-gray-100 text-black rounded">
                     <form className="flex flex-col" onSubmit={handleSubmit}>
                         <h2 className="text-center font-bold text-xl text-regGreen">FREE TRIAL LESSON</h2>
                         <p className="text-xs text-center mb-6">Get started with your free trial lesson today!</p>
@@ -80,8 +80,8 @@ return (
                         </label>
                         <label className="mb-4">
                             <span className="block text-sm">Desired Instrument:</span>
-                            <select className="w-full h-8 border-2 border-gray-300 text-sm" ref={instrumentRef} required>
-                                <option value="" disabled selected>choose instrument</option>
+                            <select className="w-full h-8 border-2 border-gray-300 text-sm" ref={instrumentRef} defaultValue="choose instrument" required>
+                                <option value="choose instrument" disabled>choose instrument</option>
                                 <option value="piano">piano</option>
                                 <option value="guitar">guitar</option>
                                 <option value="ukulele">ukulele</option>
@@ -112,8 +112,8 @@ return (
                         </label>
                         <label className="mb-4">
                             <span className="block text-sm">How did you hear about us?</span>
-                            <select className="w-full h-8 border-2 border-gray-300 text-sm" ref={sourceRef} required>
-                                <option value="" disabled selected>choose option</option>
+                            <select className="w-full h-8 border-2 border-gray-300 text-sm" ref={sourceRef} defaultValue="choose option" required>
+                                <option value="choose option" disabled>choose option</option>
                                 <option value="google">google search</option>
                                 <option value="humbertown">humbertown sign</option>
                                 <option value="humbertown">friend/relative</option>
