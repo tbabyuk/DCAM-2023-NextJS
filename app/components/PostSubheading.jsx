266ab156c7878f4258086849722fa64b1e47c0ce
Terry Babyuk <tbabyuk@gmail.com>
2023-08-29 15:56:37 -0400

    started working on 'rcm-exams-complete-guide' blog post

--- app/components/PostSubheading.jsx
+++ app/components/PostSubheading.jsx
@@ -5,9 +5,9 @@
 
 
 
-export const PostSubheading = ({subheading, bottom}) => {
+export const PostSubheading = ({subheading, align, bottom}) => {
 
     return (
-          <h3 className={`font-ovo text-regRed font-semibold px-5 lg:px-36 xl:px-64 text-2xl ${bottom}`}>{subheading}</h3>
+          <h3 className={`font-ovo text-regRed ${align} font-semibold px-5 lg:px-36 xl:px-64 text-2xl ${bottom}`}>{subheading}</h3>
       )
   }
