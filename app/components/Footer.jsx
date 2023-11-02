d923d7a54a00e6a50a02b74480f84d32f467a9e1
Terry Babyuk <tbabyuk@gmail.com>
2023-11-02 11:04:49 -0400

    added Jonathan Stuchbery link to footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,8 +6,9 @@ import { FaFacebookSquare, FaInstagramSquare, FaYoutubeSquare } from "react-icon
 
 const Footer = () => {
     return ( 
-        <footer className="mt-40 grid grid-cols-1 gap-y-14 md:grid-cols-2 lg:grid-cols-4 px-5 lg:px-12 py-8 bg-gradient-to-r from-darkBlue to-regBlue min-h-50 text-gray-50 font-roboto">
-            <div>
+        // <footer className="mt-40 grid grid-cols-1 gap-y-14 md:grid-cols-2 lg:grid-cols-4 px-5 lg:px-12 py-8 bg-gradient-to-r from-darkBlue to-regBlue min-h-50 text-gray-50 font-roboto">
+        <footer className="mt-40 px-5 lg:px-10 py-8 flex flex-col-reverse gap-y-14 md:flex-row bg-gradient-to-r from-darkBlue to-regBlue min-h-50 text-gray-50 font-roboto">
+            <div className="flex-1">
                 <h4 className="mb-6 inline-block border-b-2">FOLLOW US:</h4>
                     <div className="social-links flex">
                         <Link 
@@ -25,15 +26,21 @@ const Footer = () => {
                             <FaYoutubeSquare size="2.5em" />
                         </Link>
                     </div>
-                    <div className="mt-12">
+                    <div className="flex-1 mt-12">
                         <p className="mb-3">Educational Affiliates:</p>
-                        <a href="https://khrysmarmusic.ca/" target="_blank">
-                            <img src="khrysmar_logo_bw.png" width="70px" alt="Khrysmar Music Studio" />
-                        </a>
+                        <div className="flex flex-col text-[0.8rem] gap-2">
+                            <a href="https://khrysmarmusic.ca/" target="_blank">
+                                Khrysmar Music Studio
+                                {/* <img src="khrysmar_logo_bw.png" width="70px" alt="Khrysmar Music Studio" /> */}
+                            </a>
+                            <a href="https://www.jonathanstuchbery.com/" target="_blank">
+                                Jonathan Stuchbery
+                            </a>
+                        </div>
                     </div>
             </div>
 
-            <div>
+            <div className="flex-1">
                 <h4 className="mb-6 inline-block border-b-2">WHAT WE OFFER:</h4>
                 <ul className="text-sm leading-7 font-light">
                     <li><Link href="#" className="hover:text-lightRed">Piano Lessons</Link></li>
@@ -45,7 +52,7 @@ const Footer = () => {
                     <li><Link href="#" className="hover:text-lightRed">Intro to Music Program</Link></li>
                 </ul>
             </div>
-            <div>
+            <div className="flex-1">
                 <h4 className="mb-6 inline-block border-b-2">LEARN MORE:</h4>
                 <ul className="text-sm leading-7 font-light">
                     <li><Link href="/rates" className="hover:text-lightRed">Rates</Link></li>
@@ -58,7 +65,7 @@ const Footer = () => {
                     {/* <li><Link href="/privacypolicy" className="hover:text-lightRed">Privacy Policy</Link></li> */}
                 </ul>
             </div>
-            <div>
+            <div className="flex-1">
                 <h4 className="mb-6 inline-block border-b-2">CONTACT US:</h4>
                 <ul className="text-sm leading-10 font-light">
                     <li><Link href="tel:+4162379595" className="hover:text-lightRed">(416) 237-9595</Link></li>
