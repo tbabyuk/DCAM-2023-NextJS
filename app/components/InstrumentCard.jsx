52038e27ed441312ada303c02c27eba8740999cc
Terry Babyuk <tbabyuk@gmail.com>
2023-12-04 13:26:27 -0500

    removed blue bg from instrument slider card

--- app/components/InstrumentCard.jsx
+++ app/components/InstrumentCard.jsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 export const InstrumentCard = ({instrument, imageSrc, url}) => {
 
     return (
-        <div className="w-[250px] h-auto relative bg-blue-300 rounded-lg overflow-hidden mx-auto">
+        <div className="w-[250px] h-auto relative rounded-lg overflow-hidden mx-auto">
             <Link href={url} alt="read more"> 
                 <Image src={imageSrc} width={540} height={810} />
                 <h3 className="absolute bottom-0 left-[50%] -translate-x-[50%] text-[2.5rem] bg-black w-full bg-opacity-[40%] text-center text-gray-200">{instrument}</h3>
