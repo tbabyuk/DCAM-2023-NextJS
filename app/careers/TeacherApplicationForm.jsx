4401d9eda408d6d2cdefb4184e70e9e3017ea5f5
Terry Babyuk <tbabyuk@gmail.com>
2024-01-04 01:53:22 -0500

    made progress on applicant form validation

--- app/careers/TeacherApplicationForm.jsx
+++ app/careers/TeacherApplicationForm.jsx
@@ -3,37 +3,144 @@
 
 
 import { useState } from "react"
+import { storage } from "../firebase/config"
+import { ref, uploadBytes } from "firebase/storage"
+import { v4 as uuidv4 } from 'uuid';
 
 
 export const TeacherApplicationForm = () => {
 
-  const [teacherName, setTeacherName] = useState("")
-  const [parentName, setParentName] = useState("")
+  const [fullName, setFullName] = useState("")
   const [phone, setPhone] = useState("")
   const [email, setEmail] = useState("")
-  const [instrument, setInstrument] = useState("choose instrument")
-  const [lessonType, setLessonType] = useState("private")
-  const [lessonDuration, setLessonDuration] = useState("30")
-  const [lessonFrequency, setLessonFrequency] = useState("once-a-week")
-  const [lessonDays, setLessonDays] = useState({
-    "monday": false,
-    "tuesday": false,
-    "wednesday": false,
-    "thursday": false,
-    "saturday": false
+  const [instruments, setInstruments] = useState({
+    "piano": false,
+    "guitar": false,
+    "drums": false,
+    "vocals": false,
+    "toddlers": false
   })
+  const [typeOfWork, setTypeOfWork] = useState({
+    "permanent": false,
+    "substitute": false
+  })
+  const [teachingExperience, setTeachingExperience] = useState("choose option")
   const [source, setSource] = useState("choose option")
   const [comments, setComments] = useState("")
+  const [resume, setResume] = useState(null)
+  const [success, setSuccess] = useState(null)
+  const [fullNameError, setFullNameError] = useState(null)
+  const [phoneError, setPhoneError] = useState(null)
+  const [emailError, setEmailError] = useState(null)
+  const [instrumentsError, setInstrumentsError] = useState(null)
+  const [typeOfWorkError, setTypeOfWorkError] = useState(null)
+  const [teachingExperienceError, setTeachingExperienceError] = useState(null)
+  const [sourceError, setSourceError] = useState(null)
+  const [resumeError, setResumeError] = useState(null)
+  const [error, setError] = useState(null)
+  const [uploadError, setUploadError] = useState(null)
 
 
-  const handleLessonDays = (day) => {
-    setLessonDays({...lessonDays, [day]: !lessonDays[day]})
+  console.log("storage:", storage)
+
+
+  const handleInstruments = (instrument) => {
+    setInstruments({...instruments, [instrument]: !instruments[instrument]})
+  }
+
+  const handleTypeOfWork = (type) => {
+    setTypeOfWork({...typeOfWork, [type]: !typeOfWork[type]})
   }
 
 
-  const [submitting, setSubmitting] = useState(false)
+//   const [submitting, setSubmitting] = useState(false)
   const [showSuccessResponse, setShowSuccessResponse] = useState(false)
-  const [showErrorResponse, setShowErrorResponse] = useState(false)
+//   const [showErrorResponse, setShowErrorResponse] = useState(false)
+
+
+
+  const handleResumeUpload = (e) => {
+    const allowedTypes = ["application/msword", "application/pdf"]
+    const file = e.target.files[0]
+    
+    if(file && allowedTypes.includes(file.type)) {
+        setResume(file)
+        setUploadError(null)
+    } else {
+        setResume(null)
+        console.log("that file type is not allowed")
+        setUploadError("You can only upload Microsoft Word or PDF files")
+    }
+  }
+
+
+  const validateFields = () => {
+
+
+    // validate full name
+    const fullNameRegex = /^[a-zA-Z ]{6,40}$/
+    const fullNamePass = fullNameRegex.test(fullName)
+    if(!fullNamePass) {
+        setFullNameError("Please enter a valid full name")
+    } else {
+        setFullNameError(null)
+    }
+
+    // validate phone number
+    const phoneRegex = /^[\d\s()-]{10,20}$/
+    const phonePass = phoneRegex.test(phone)
+    if(!phonePass) {
+        setPhoneError("Please enter a valid phone number")
+    } else {
+        setPhoneError(null)
+    }
+
+    // validate email
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    const emailPass = emailRegex.test(email)
+    if(!emailPass) {
+        setEmailError("Please enter a valid email")
+    } else {
+        setEmailError(null)
+    }
+
+
+
+
+    // const instrumentsValues = Object.values(instruments)
+    // if(!instrumentsValues.includes(true)) {
+    //     setInstrumentsError("You must select at least one instrument that you can teach")
+    // } else {
+    //     setInstrumentsError(null)
+    // }
+
+    // const typeOfWorkValues = Object.values(typeOfWork)
+    // if(!typeOfWorkValues.includes(true)) {
+    //     setTypeOfWorkError("You must select at least one type of work you are interested in")
+    // } else {
+    //     setTypeOfWorkError(null)
+    // }
+
+    // if(teachingExperience === "choose option") {
+    //     setTeachingExperienceError("Please indicate how much teaching experience you have")
+    // } else {
+    //     setTeachingExperienceError(null)
+    // }
+
+    // if(source === "choose option") {
+    //     setSourceError("Please indicate how you heard about our school")
+    // } else {
+    //     setSourceError(null)
+    // }
+
+    // if(!resume) {
+    //     setResumeError("You must upload your resume in order to complete the application")
+    // } else {
+    //     setResumeError(null)
+    // }
+
+
+  }
   
 
 
@@ -41,41 +148,79 @@ export const TeacherApplicationForm = () => {
 
     e.preventDefault()
 
-    setSubmitting(true)
-    const response = await fetch("/api/register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            student: studentName.trim(),
-            parent: parentName.trim(),
-            phone: phone.trim(),
-            email: email.trim(),
-            instrument,
-            lessonType,
-            lessonDuration,
-            lessonFrequency,
-            lessonDays,
-            source,
-            comments: comments.trim()
-        })
-    })
+    validateFields()
 
 
-    if(response.status === 200) {
-        console.log("status is:", response.status)
-        const responseData = await response.json()
-        console.log("responseData from ok:", responseData.message)
-        setSubmitting(false)
-        setShowSuccessResponse(true)
-    } else {
-        console.log("status is:", response.status)
-        const responseData = await response.json()
-        console.log("responseData from error:", responseData.error)
-        setSubmitting(false)
-        setShowErrorResponse(true)
-    }
+
+    // upload resume to storage
+
+    // const resumeRef = ref(storage, `resumes/${uuidv4()}___${resume.name}`);
+
+    // try {
+    //     uploadBytes(resumeRef, resume)
+    //     setSuccess("Image uploaded successfully!")
+    // } catch {
+    //     setError("Oops, something went wrong with the upload!")
+    // }
+
+
+    // console.log(name, phone, email, instruments, typeOfWork, teachingExperience, source, comments)
+
+
+
+
+
+
+
+
+
+
+
+    // setSubmitting(true)
+    // const response = await fetch("/api/register", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify({
+    //         student: studentName.trim(),
+    //         parent: parentName.trim(),
+    //         phone: phone.trim(),
+    //         email: email.trim(),
+    //         instrument,
+    //         lessonType,
+    //         lessonDuration,
+    //         lessonFrequency,
+    //         lessonDays,
+    //         source,
+    //         comments: comments.trim()
+    //     })
+    // })
+
+
+    // if(response.status === 200) {
+    //     console.log("status is:", response.status)
+    //     const responseData = await response.json()
+    //     console.log("responseData from ok:", responseData.message)
+    //     setSubmitting(false)
+    //     setShowSuccessResponse(true)
+    // } else {
+    //     console.log("status is:", response.status)
+    //     const responseData = await response.json()
+    //     console.log("responseData from error:", responseData.error)
+    //     setSubmitting(false)
+    //     setShowErrorResponse(true)
+    // }
+
+
+
+
+
+
+
+
+
+
 
 
     // console.log(
@@ -96,7 +241,6 @@ export const TeacherApplicationForm = () => {
   return (
         <>
 
-
             {showSuccessResponse ? (
                         <div className="h-full text-green-600 px-5 self-center flex flex-col text-center">           
                                 <p className="mb-6">Your form was successfully submitted and will be in our records - thank you!</p>
@@ -107,84 +251,85 @@ export const TeacherApplicationForm = () => {
                 <h2 className="text-center font-bold text-xl text-regGreen">TEACHER APPLICATION FORM</h2>
                 <p className="text-xs text-center mb-10">Start your teaching career with DCAM today!</p>
 
-                <label className="mb-6">
-                    <span className="block text-sm">Name:</span>
+                <label className="mb-1">
+                    <span className="block text-sm">Full Name:</span>
                     <input 
                         type="text" 
-                        className="w-full h-8 border-2 border-gray-300 ps-2 text-sm" 
-                        autoFocus
-                        required
-                        onChange={(e) => setTeacherName(e.target.value)}
-                        value={teacherName}
+                        className={`w-full h-8 border-2 ${fullNameError ? "border-red-500" : "border-gray-300"} ps-2 text-sm`} 
+                        onChange={(e) => setFullName(e.target.value)}
+                        value={fullName}
                     />
+                    <span className={`text-[0.8rem] text-right text-red-500 h-[20px] block`}>{fullNameError && fullNameError}</span>
                 </label>
 
-                <label className="mb-4">
+                <label className="mb-1">
                     <span className="block text-sm">Phone:</span>
                     <input 
                         type="tel"
-                        className="w-full h-8 border-2 border-gray-300 ps-2 text-sm"
-                        required
+                        className={`w-full h-8 border-2 ${phoneError ? "border-red-500" : "border-gray-300"} ps-2 text-sm`}
                         onChange={(e) => setPhone(e.target.value)}
                         value={phone}
                     />
+                    <span className={`text-[0.8rem] text-right text-red-500 h-[20px] block`}>{phoneError && phoneError}</span>
                 </label>
-                <label className="mb-4">
+                <label className="mb-1">
                     <span className="block text-sm">Email:</span>
                     <input 
                         type="email"
-                        className="w-full h-8 border-2 border-gray-300 ps-2 text-sm"
-                        required
+                        className={`w-full h-8 border-2 ${emailError ? "border-red-500" : "border-gray-300"} ps-2 text-sm`}
                         onChange={(e) => setEmail(e.target.value)}
                         value={email}
                     />
+                    <span className={`text-[0.8rem] text-right text-red-500 h-[20px] block`}>{emailError && emailError}</span>
+
                 </label>
 
-                <div className="mb-6 border-2 border-gray-300 p-4">
+                <div className={`mb-6 border-2 ${instrumentsError ? "border-red-600" : "border-gray-300"}  p-4`}>
                     <span className="block text-sm">INSTRUMENT:</span>
                     <span className="text-[0.8rem] font-semibold block mb-4">What instrument(s) do you specialize in that you would like to teach?</span>
                     <label className="flex items-center text-[0.9rem] mb-4">
-                        <input type="checkbox" id="piano" value="piano" checked={lessonDays.piano} onChange={(e) => handleLessonDays(e.target.value)} className="me-1" />Piano
+                        <input type="checkbox" id="piano" value="piano" checked={instruments.piano} onChange={(e) => handleInstruments(e.target.value)} className="me-1" />Piano
                     </label>
                     <label className="flex items-center text-[0.9rem] mb-4">
-                        <input type="checkbox" id="guitar" value="guitar" checked={lessonDays.guitar} onChange={(e) => handleLessonDays(e.target.value)}className="me-1" />Guitar
+                        <input type="checkbox" id="guitar" value="guitar" checked={instruments.guitar} onChange={(e) => handleInstruments(e.target.value)}className="me-1" />Guitar
                     </label>
                     <label className="flex items-center text-[0.9rem] mb-4">
-                        <input type="checkbox" id="drums" value="drums" checked={lessonDays.drums} onChange={(e) => handleLessonDays(e.target.value)} className="me-1" />Drums
+                        <input type="checkbox" id="drums" value="drums" checked={instruments.drums} onChange={(e) => handleInstruments(e.target.value)} className="me-1" />Drums
                     </label>
                     <label className="flex items-center text-[0.9rem] mb-4">
-                        <input type="checkbox" id="vocals" value="vocals" checked={lessonDays.vocals} onChange={(e) => handleLessonDays(e.target.value)} className="me-1" />Vocals
+                        <input type="checkbox" id="vocals" value="vocals" checked={instruments.vocals} onChange={(e) => handleInstruments(e.target.value)} className="me-1" />Vocals
                     </label>
                     <label className="flex items-center text-[0.9rem] mb-4">
-                        <input type="checkbox" id="toddlers" value="toddlers" checked={lessonDays.toddlers} onChange={(e) => handleLessonDays(e.target.value)} className="me-1" />Music for Toddlers
+                        <input type="checkbox" id="toddlers" value="toddlers" checked={instruments.toddlers} onChange={(e) => handleInstruments(e.target.value)} className="me-1" />Music for Toddlers
                     </label>
                 </div>
 
-                <div className="mb-6 border-2 border-gray-300 p-4">
+                <div className={`mb-6 border-2 ${typeOfWorkError ? "border-red-600" : "border-gray-300" } p-4`}>
                     <span className="block text-sm">TYPE OF WORK:</span>
                     <span className="text-[0.8rem] font-semibold block mb-4">What kind of teaching position are you interested in at the moment?</span>
 
                     <label className="flex items-center text-[0.9rem] mb-4">
-                        <input type="checkbox" id="vocals" value="vocals" checked={lessonDays.vocals} onChange={(e) => handleLessonDays(e.target.value)} className="me-1" />Permanent (regular weekly lessons)
+                        <input type="checkbox" id="permanent" value="permanent" checked={typeOfWork.permanent} onChange={(e) => handleTypeOfWork(e.target.value)} className="me-1" />Permanent (regular weekly lessons)
                     </label>
                     <label className="flex items-center text-[0.9rem] mb-4">
-                        <input type="checkbox" id="toddlers" value="toddlers" checked={lessonDays.toddlers} onChange={(e) => handleLessonDays(e.target.value)} className="me-1" />Substitute (we call you when we need you)
+                        <input type="checkbox" id="substitute" value="substitute" checked={typeOfWork.substitute} onChange={(e) => handleTypeOfWork(e.target.value)} className="me-1" />Substitute (we call you when we need you)
                     </label>
                 </div>
 
                 <label className="mb-6">
                     <span className="block text-sm">How many years of teaching experience do you have:</span>
-                    <select className="w-full h-8 border-2 border-gray-300 text-sm" value={instrument} onChange={(e) => {setInstrument(e.target.value)}} required>
-                        <option value="choose instrument" disabled>&lt; 1 year</option>
-                        <option value="choose instrument">1 - 2 years</option>
-                        <option value="choose instrument">2 - 3 years</option>
-                        <option value="choose instrument">&gt; 3 years</option>
+                    <select className={`w-full h-8 border-2 ${teachingExperienceError ? "border-red-600" : "border-gray-300"} text-sm`} value={teachingExperience} onChange={(e) => {setTeachingExperience(e.target.value)}} required>
+                        <option value="choose option" disabled>choose option</option>
+                        <option value="less than 1 year">&lt; 1 year</option>
+                        <option value="1 to 2 years">1 - 2 years</option>
+                        <option value="2 to 3 years">2 - 3 years</option>
+                        <option value="more than 3 years">&gt; 3 years</option>
                     </select>
                 </label>
 
                 <label className="mb-4">
                     <span className="block text-sm">How did you hear about us?</span>
-                    <select className="w-full h-8 border-2 border-gray-300 text-sm" value={source} onChange={e => setSource(e.target.value)} required>
+                    <select className={`w-full h-8 border-2 ${sourceError ? "border-red-600" : "border-gray-300"} text-sm`} value={source} onChange={e => setSource(e.target.value)} required>
                         <option value="choose option" disabled>choose option</option>
                         <option value="google">google search</option>
                         <option value="humbertown">humbertown sign</option>
@@ -204,20 +349,29 @@ export const TeacherApplicationForm = () => {
 
                 <label className="mb-4">
                     <span className="block text-sm mb-4">Upload your resume:</span>
-                    <input type="file" id="myFile" name="filename" />
+                        <input type="file" id="myFile" name="filename" className={`${resumeError ? "border-2 border-red-600" : ""}`} onChange={handleResumeUpload} />
+                    {uploadError && <p className="text-red-600 text-sm">{uploadError}</p>}
                 </label>
 
-                <button className="dcam-button w-full mt-3 h-10" disabled={submitting}>{submitting ? "Submitting...Please wait..." : "Submit"}</button>
+                {/* <button className="dcam-button w-full mt-3 h-10" disabled={submitting}>{submitting ? "Submitting...Please wait..." : "Submit"}</button> */}
+                <button className="dcam-button w-full mt-3 h-10">Submit</button>
+                {success && <p className="text-green-600">{success}</p>}
+                {error && <p className="text-red-600">{error}</p>}
+                {instrumentsError && <p className="text-red-600">{instrumentsError}</p>}
+                {typeOfWorkError && <p className="text-red-600">{typeOfWorkError}</p>}
+                {teachingExperienceError && <p className="text-red-600">{teachingExperienceError}</p>}
+                {sourceError && <p className="text-red-600">{sourceError}</p>}
+                {resumeError && <p className="text-red-600">{resumeError}</p>}
             </form>
 
             ) 
         }
 
-            {showErrorResponse && (
+            {/* {showErrorResponse && (
                 <div className="h-full text-red-600 px-5 self-center flex flex-col text-center">           
                         <p className="mb-6">Sorry, there was a problem submitting your form. Please refresh and try again or send us a direct email at <span className="font-bold">info@dacapomusic.ca</span></p>
                 </div>
-            )}
+            )} */}
 
         </>
     )
