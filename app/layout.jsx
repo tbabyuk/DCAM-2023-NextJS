95594e2edc631937ccafa739410f213c37bea350
Terry Babyuk <tbabyuk@gmail.com>
2024-01-27 02:28:32 -0500

    added Ezoic script to layout.jsx file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,5 @@
 import './globals.css'
 import Footer from './components/Footer'
-import Header from './components/Header'
 import { GoogleAnalytics } from './components/GoogleAnalytics'
 import { Ovo, Roboto } from 'next/font/google'
 import { ShopContextProvider } from './context/ShopContext'
@@ -8,7 +7,8 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { TopHeader } from './components/TopHeader'
 import Navigation from './components/Navigation'
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 
 
 const ovo = Ovo({
@@ -38,6 +38,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" style={{scrollBehavior: "smooth"}} className={`${ovo.variable} ${roboto.variable} text-gray-600`}>
+      <head>
+          <Script src="//www.ezojs.com/basicads.js?d=dacapomusic.ca" />
+      </head>
       <GoogleAnalytics GA_MEASUREMENT_ID={process.env.GA_MEASUREMENT_ID} />
       <ShopContextProvider>
         <body>
