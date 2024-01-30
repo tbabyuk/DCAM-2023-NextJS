e04c2bb8e626986bed88cca976b95167271f75a1
Terry Babyuk <tbabyuk@gmail.com>
2024-01-30 12:00:53 -0500

    successfully displaying html for exam data, improved mobile responsivness

--- app/tools/rcm-wizard/page.jsx
+++ app/tools/rcm-wizard/page.jsx
@@ -21,8 +21,8 @@ function RcmWizard() {
 
 
   return (
-    <div className="flex h-[80vh]">
-        <aside className="bg-gray-200 p-6 w-[420px]">
+    <div className="flex h-auto">
+        <aside className="bg-gray-200 h-fit p-6 w-[350px] min-w-[350px]">
             <h4>Choose RCM Level to view requirements</h4>
                 <select name="" id="" className="w-full my-6 h-12 rounded-md text-center cursor-pointer border-2 border-gray-400" onChange={handleChange}>
                     <option value="">Select RCM Level</option>
@@ -45,22 +45,22 @@ function RcmWizard() {
         </aside>
         <main className="w-full">
             <h1 className="w-full py-6 text-[1.8rem] text-center">Requirements for:</h1>
-            <div className="flex gap-3">
-                <div className="flex-1">
+            <div className="flex flex-wrap px-8 gap-3">
+                <div className="w-[250px] flex-shrink-0">
                     <h4 className="bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400">Technical Requirements</h4>
-                    <p>{technicalRequirements && technicalRequirements}</p>
+                    {technicalRequirements && <p dangerouslySetInnerHTML={{__html: technicalRequirements}}></p>}
                 </div>
-                <div className="flex-1">
+                <div className="w-[250px] flex-shrink-0">
                     <h4 className=" bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400">Repertoire</h4>
-                    <p>{repertoire && repertoire}</p>
+                    {repertoire && <p dangerouslySetInnerHTML={{__html: repertoire}}></p>}
                 </div>
-                <div className="flex-1">
-                    <h4 className=" bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400">Etudes</h4>
-                    <p>{etudes && etudes}</p>
+                <div className="w-[250px] flex-shrink-0">
+                    <h4 className="bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400">Etudes</h4>
+                    {etudes && <p dangerouslySetInnerHTML={{__html: etudes}}></p>}
                 </div>
-                <div className="flex-1">
-                    <h4 className=" bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400">Sight Reading & Ear Tests</h4>
-                    <p>{earTests && earTests}</p>
+                <div className="w-[250px] flex-shrink-0">
+                    <h4 className="bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400">Sight Reading & Ear Tests</h4>
+                    {earTests && <p dangerouslySetInnerHTML={{__html: earTests}}></p>}
                 </div>
             </div>
         </main>
