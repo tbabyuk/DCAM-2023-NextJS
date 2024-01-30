de1a0db8a74d5e95451930cd75a76c70b66042b0
Terry Babyuk <tbabyuk@gmail.com>
2024-01-30 15:05:08 -0500

    header nearly completed

--- app/tools/rcm-wizard/layout.js
+++ app/tools/rcm-wizard/layout.js
@@ -3,7 +3,11 @@
 const ToolsLayout = ({ children }) => {
     return (
         <>
-            <h1 className="font-ovo text-white text-4xl px-5 py-14 lg:px-36 xl:px-52 bg-blue-600 bg-center bg-cover">TOOLS - RCM WIZARD</h1>
+            <div className="flex justify-between items-center text-white text-4xl px-5 py-8 lg:px-36 xl:px-52 bg-gradient-to-r from-[#241773] to-regBlue bg-center bg-cover">
+                <img src="/images/tools/rcm-logo.png" width="110px" />
+                <h1>TOOLS - RCM EXAM WIZARD</h1>
+                <img src="/images/tools/dcam-logo-white.png" width="110px" />
+            </div>
             {children}
         </>
     )
