d55967643a5aa3939bf9b7f9f4bbac224226ef68
Terry Babyuk <tbabyuk@gmail.com>
2024-01-30 23:56:11 -0500

    completed RCM Wizard tool

--- app/tools/rcm-wizard/layout.js
+++ app/tools/rcm-wizard/layout.js
@@ -3,9 +3,12 @@
 const ToolsLayout = ({ children }) => {
     return (
         <>
-            <div className="flex justify-between items-center text-white text-4xl px-5 py-8 lg:px-36 xl:px-52 bg-gradient-to-r from-[#241773] to-regBlue bg-center bg-cover">
+            <div className="flex flex-col-reverse gap-9 md:flex-row justify-between items-center text-white px-5 py-8 lg:px-36 xl:px-52 bg-gradient-to-r from-[#241773] to-regBlue bg-center bg-cover">
                 <img src="/images/tools/rcm-logo.png" width="110px" />
-                <h1>TOOLS - RCM EXAM WIZARD</h1>
+                <div>
+                    <h1 className="text-center text-4xl">RCM Exam Wizard</h1>
+                    <p className="text-center font-light">See basic exam requirements for all RCM piano levels</p>
+                </div>
                 <img src="/images/tools/dcam-logo-white.png" width="110px" />
             </div>
             {children}
--- app/tools/rcm-wizard/page.jsx
+++ app/tools/rcm-wizard/page.jsx
@@ -1,18 +1,29 @@
 "use client"
 
 import { useState } from "react"
-
 import { LEVELS } from "./levelsData"
 
-function RcmWizard() {
+function RCMWizard() {
 
+  const [level, setLevel] = useState("")
   const [technicalRequirements, setTechnicalRequirements] = useState("")
   const [repertoire, setRepertoire] = useState("")
   const [etudes, setEtudes] = useState("")
   const [earTests, setEarTests] = useState("")
 
+
   const handleChange = (e) => {
     const targetLevel = e.target.value
+    if(targetLevel === "none") {
+        setLevel("No level selected")
+        setTechnicalRequirements("")
+        setRepertoire("")
+        setEtudes("")
+        setEarTests("")
+        return
+    } else {
+        setLevel(LEVELS[targetLevel].level)
+    }
     setTechnicalRequirements(LEVELS[targetLevel].technical)
     setRepertoire(LEVELS[targetLevel].repertoire)
     setEtudes(LEVELS[targetLevel].etudes)
@@ -21,11 +32,11 @@ function RcmWizard() {
 
 
   return (
-    <div className="flex h-auto">
-        <aside className="bg-gray-200 h-fit p-6 w-[350px] min-w-[350px]">
-            <h4>Choose RCM Level to view requirements</h4>
+    <div className="flex flex-col sm:flex-row h-auto">
+        <aside className="bg-gray-200 h-fit p-6 w-full md:max-w-[320px]">
+            <h4 className="text-center">Choose Level to view requirements</h4>
                 <select name="" id="" className="w-full my-6 h-12 rounded-md text-center cursor-pointer border-2 border-gray-400" onChange={handleChange}>
-                    <option value="">Select RCM Level</option>
+                    <option value="none">Select RCM Level</option>
                     <option value="prepA">Preparatory A</option>
                     <option value="prepB">Preparatory B</option>
                     <option value="lvl1">Level 1</option>
@@ -39,28 +50,29 @@ function RcmWizard() {
                     <option value="lvl9">Level 9</option>
                     <option value="lvl10">Level 10</option>
                 </select>
-                <p className="mb-2 text-sm text-gray-500">Source: <span className="italic">RCM Piano Syllabus, 2022 Edition</span></p>
-                <p className="mb-2 text-sm text-gray-500">*This tool is designed as a general guide only. For more detailed information on the different sections of the exam, feel free to consult the RCM Piano Syllabus.</p>
-                <p className="mb-2 text-sm text-gray-500">**The wizard currently displays exam information for piano only. Other instruments will be added in the future depending on demand.</p>
+                <p className="mb-2 text-sm text-gray-400">Source: <span className="italic">RCM Piano Syllabus, 2022 Edition</span></p>
+                <p className="mb-2 text-sm text-gray-400">*This tool is designed as a general guide only. For more detailed information on the different sections of the exam, feel free to consult the RCM Piano Syllabus.</p>
+                <p className="mb-2 text-sm text-gray-400">**The wizard currently displays exam information for piano only. Other instruments will be added in the future depending on demand.</p>
         </aside>
         <main className="w-full">
-            <h1 className="w-full py-6 text-[1.8rem] text-center">Requirements for:</h1>
-            <div className="flex flex-wrap px-8 gap-3">
-                <div className="w-[250px] flex-shrink-0">
-                    <h4 className="bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400">Technical Requirements</h4>
-                    {technicalRequirements && <p dangerouslySetInnerHTML={{__html: technicalRequirements}}></p>}
+            <h1 className="w-full py-6 text-[1.8rem] text-center">Viewing requirements for: {level && <span dangerouslySetInnerHTML={{__html: level }} className="font-bold"></span>}</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-4 px-8 gap-3">
+                {/* <div className="w-full md:max-w-[250px]"> */}
+                <div className="">
+                    <h4 className="bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400  mb-4">Technical Requirements</h4>
+                    {technicalRequirements && <p className="mb-6" dangerouslySetInnerHTML={{__html: technicalRequirements}}></p>}
                 </div>
-                <div className="w-[250px] flex-shrink-0">
-                    <h4 className=" bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400">Repertoire</h4>
-                    {repertoire && <p dangerouslySetInnerHTML={{__html: repertoire}}></p>}
+                <div className="">
+                    <h4 className="bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400 mb-4">Repertoire</h4>
+                    {repertoire && <p className="mb-6" dangerouslySetInnerHTML={{__html: repertoire}}></p>}
                 </div>
-                <div className="w-[250px] flex-shrink-0">
-                    <h4 className="bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400">Etudes</h4>
-                    {etudes && <p dangerouslySetInnerHTML={{__html: etudes}}></p>}
+                <div className="">
+                    <h4 className="bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400 mb-4">Etudes</h4>
+                    {etudes && <p className="mb-6" dangerouslySetInnerHTML={{__html: etudes}}></p>}
                 </div>
-                <div className="w-[250px] flex-shrink-0">
-                    <h4 className="bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400">Sight Reading & Ear Tests</h4>
-                    {earTests && <p dangerouslySetInnerHTML={{__html: earTests}}></p>}
+                <div className="">
+                    <h4 className="bg-gray-200 text-[1.1rem] py-1 text-center border-b-2 border-gray-400 mb-4">Sight Reading & Ear Tests</h4>
+                    {earTests && <p className="mb-6" dangerouslySetInnerHTML={{__html: earTests}}></p>}
                 </div>
             </div>
         </main>
@@ -68,4 +80,4 @@ function RcmWizard() {
   )
 }
 
-export default RcmWizard
+export default RCMWizard
