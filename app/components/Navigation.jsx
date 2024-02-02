16d8d191759e0743098dafed57c250e08032b776
Terry Babyuk <tbabyuk@gmail.com>
2024-02-01 22:02:32 -0500

    finished styling mobile dropdown menu

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -26,8 +26,8 @@ return (
         </div>
         <ul
         className={`${isOpen ? "block absolute bg-regBlue w-full" : "hidden"} md:static md:flex md:flex-wrap md:justify-center text-gray-50 z-[5] text-[0.95rem]`}>
-            <Link href="/" onClick={() => setIsOpen(false)}><li className="menu-item">Home</li></Link>
-            <li className="dropdown-menu-item relative cursor-pointer" onMouseEnter={() => setSubnav1IsOpen(true)} onMouseLeave={() => setSubnav1IsOpen(false)} onClick={() => setSubnav1IsOpen(!subnav1IsOpen)}>Lessons<IoIosArrowDown className="ms-1" />
+            <Link href="/" onClick={() => setIsOpen(false)}><li className="menu-item border-b-2 border-gray-200 md:border-none">Home</li></Link>
+            <li className="dropdown-menu-item relative cursor-pointer border-b-2 border-gray-200 md:border-none" onMouseEnter={() => setSubnav1IsOpen(true)} onMouseLeave={() => setSubnav1IsOpen(false)} onClick={() => setSubnav1IsOpen(!subnav1IsOpen)}>Lessons<IoIosArrowDown className="ms-1" />
                 {subnav1IsOpen && (
                     <ul className="absolute bottom-0 left-0 translate-y-[100%] w-full md:w-[220px] flex flex-col bg-regBlue text-[0.95rem] z-[6]">
                         <Link href="/piano-lessons-etobicoke" onClick={() => setIsOpen(false)}><li className="submenu-item">Piano Lessons</li></Link>
@@ -40,15 +40,15 @@ return (
                     </ul>
                 )}
             </li>
-            <Link href="/rates" className="hover:bg-purple-500" onClick={() => setIsOpen(false)}><li className="menu-item">Rates</li></Link>
-            <Link href="/teachers" onClick={() => setIsOpen(false)}><li className="menu-item">Teachers</li></Link>
-            <Link href="/reviews" onClick={() => setIsOpen(false)}><li className="menu-item">Reviews</li></Link>
+            <Link href="/rates" className="hover:bg-purple-500" onClick={() => setIsOpen(false)}><li className="menu-item border-b-2 border-gray-200 md:border-none">Rates</li></Link>
+            <Link href="/teachers" onClick={() => setIsOpen(false)}><li className="menu-item border-b-2 border-gray-200 md:border-none">Teachers</li></Link>
+            <Link href="/reviews" onClick={() => setIsOpen(false)}><li className="menu-item border-b-2 border-gray-200 md:border-none">Reviews</li></Link>
             {/* <Link href="/gallery" onClick={() => setIsOpen(false)}><li className="menu-item">Gallery</li></Link> */}
-            <Link href="/about" onClick={() => setIsOpen(false)}><li className="menu-item">About</li></Link>
-            <Link href="/contact" onClick={() => setIsOpen(false)}><li className="menu-item">Contact</li></Link>
-            <Link href="/blog" onClick={() => setIsOpen(false)}><li className="menu-item">Blog</li></Link>
-            <Link href="/careers" onClick={() => setIsOpen(false)}><li className="menu-item">Careers</li></Link>
-            <li className="dropdown-menu-item relative cursor-pointer" onMouseEnter={() => setSubnav2IsOpen(true)} onMouseLeave={() => setSubnav2IsOpen(false)} onClick={() => setSubnav2IsOpen(!subnav2IsOpen)}>Tools<IoIosArrowDown className=" ms-1" />
+            <Link href="/about" onClick={() => setIsOpen(false)}><li className="menu-item border-b-2 border-gray-200 md:border-none">About</li></Link>
+            <Link href="/contact" onClick={() => setIsOpen(false)}><li className="menu-item border-b-2 border-gray-200 md:border-none">Contact</li></Link>
+            <Link href="/blog" onClick={() => setIsOpen(false)}><li className="menu-item border-b-2 border-gray-200 md:border-none">Blog</li></Link>
+            <Link href="/careers" onClick={() => setIsOpen(false)}><li className="menu-item border-b-2 border-gray-200 md:border-none">Careers</li></Link>
+            <li className="dropdown-menu-item relative cursor-pointer border-b-2 border-gray-200 md:border-none" onMouseEnter={() => setSubnav2IsOpen(true)} onMouseLeave={() => setSubnav2IsOpen(false)} onClick={() => setSubnav2IsOpen(!subnav2IsOpen)}>Tools<IoIosArrowDown className=" ms-1" />
                 {subnav2IsOpen && (
                     <ul className="absolute bottom-0 left-0 translate-y-[100%] w-full md:w-[220px] flex flex-col bg-regBlue text-[0.95rem] z-[6]">
                         <Link href="/tools/rcm-wizard" onClick={() => setIsOpen(false)}><li className="submenu-item">RCM Wizard</li></Link>
