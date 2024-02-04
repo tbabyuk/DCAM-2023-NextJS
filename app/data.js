d48ec063892b7bb963e39b29af8ffe1c7a106c24
Terry Babyuk <tbabyuk@gmail.com>
2024-02-04 00:23:38 -0500

    updated product categories images

--- app/data.js
+++ app/data.js
@@ -3,12 +3,12 @@
 export const shopProductCategoriesArray = [
     {
         category: "RCM BOOKS",
-        source: "https://firebasestorage.googleapis.com/v0/b/dcam-website.appspot.com/o/shop_images%2Fproduct_categories%2Frcm_books.jpg?alt=media&token=e05c52aa-5ebb-487b-ae58-75b16da783bf",
+        source: "https://firebasestorage.googleapis.com/v0/b/dcam-website.appspot.com/o/shop_images%2Fproduct_categories%2Frcm_books_category.jpg?alt=media&token=ae63b788-5431-4344-a789-92245b852284",
         productPage: "/shop/books"
     },
     {
         category: "GIFT CERTIFICATES",
-        source: "https://firebasestorage.googleapis.com/v0/b/dcam-website.appspot.com/o/shop_images%2Fproduct_categories%2Fgift_certificates.jpg?alt=media&token=ff09959d-d35f-4390-bb2f-085b65c87105",
+        source: "https://firebasestorage.googleapis.com/v0/b/dcam-website.appspot.com/o/shop_images%2Fproduct_categories%2Fgift_certificates_category.jpg?alt=media&token=dc251ece-8f9c-438f-9980-01edd7d91374",
         productPage: "/shop/gift-certificates"
     }
 ]
