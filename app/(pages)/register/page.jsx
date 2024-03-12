6ea8bcb1c33d2729e01f8df2f7b80e8cfd4db6f6
Terry Babyuk <tbabyuk@gmail.com>
2024-03-12 19:02:45 -0400

    compile error fixed on register page

--- app/(pages)/register/page.jsx
+++ app/(pages)/register/page.jsx
@@ -1,5 +1,5 @@
 import { RegisterForm } from "./RegisterForm";
-import { PageTitle } from "../components/PageTitle";
+import { PageTitle } from "@/app/components/PageTitle";
 
 
 export const metadata = {
@@ -34,7 +34,8 @@ const Register = () => {
     
     return (
             <main className="register-page">
-                <PageTitle title="Register" image="/images/page-headers/register-page.jpg" />
+                <PageTitle title="Register" image="/images/illustrations/register-page-illustration.png" alt="register page illustration" />
+
                 <p className="leading-8 px-5 lg:px-36 xl:px-52 py-12 bg-gray-100">Register for lessons today! Just fill out the registration form below and we will be in touch with you within 1-2 business days about starting your lessons!</p>
                 <section className="register-page px-5 lg:px-36 xl:px-52 py-20 mb-24">
                     <RegisterForm />
