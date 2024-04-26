ff1fd0d127d3767345dc5ef7fe1a055ba21e4fe0
Terry Babyuk <tbabyuk@gmail.com>
2024-04-26 19:55:06 -0400

    og issue fix attempt 3

--- app/(pages)/shop/books/piano/[slug]/page.jsx
+++ app/(pages)/shop/books/piano/[slug]/page.jsx
@@ -19,7 +19,7 @@ export async function generateMetadata({params}) {
     openGraph: {
       title: `${targetBook.title} | Da Capo Academy of Music Shop`,
       description: targetBook.description,
-      url: targetBook.source,
+      url: `https://www.dacapomusic.ca/shop/books/piano/${params.slug}`,
       images: [
         {
           url: targetBook.source,
